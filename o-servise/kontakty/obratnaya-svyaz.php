cd7b702905f62f4c1de000fe907d9922cfd09afa
Stolyarov Alexander <cfcf.01@mail.ru>
2024-08-22 14:09:16 +0300

    Внидрение верски + №8

--- o-servise/kontakty/obratnaya-svyaz.php
+++ o-servise/kontakty/obratnaya-svyaz.php
@@ -1,7 +1,12 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Обратная связь");
-?><?$APPLICATION->IncludeComponent("bitrix:main.feedback", "svaz", Array(
+?>
+<div class="site-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 col-lg-8 mb-5">
+<?$APPLICATION->IncludeComponent("bitrix:main.feedback", "svaz", Array(
 	"EMAIL_TO" => "cfcf.01@mail.ru",	// E-mail, на который будет отправлено письмо
 		"EVENT_MESSAGE_ID" => "",	// Почтовые шаблоны для отправки письма
 		"OK_TEXT" => "Спасибо, ваше сообщение принято.",	// Сообщение, выводимое пользователю после отправки
@@ -9,12 +14,22 @@ $APPLICATION->SetTitle("Обратная связь");
 		"USE_CAPTCHA" => "Y",	// Использовать защиту от автоматических сообщений (CAPTCHA) для неавторизованных пользователей
 	),
 	false
-);?><?$APPLICATION->IncludeComponent(
-	"bitrix:main.include",
-	"",
-	Array(
-		"AREA_FILE_SHOW" => "page",
+);?>
+</div>
+<?$APPLICATION->IncludeComponent(
+	"bitrix:main.include", 
+	".default", 
+	array(
+		"AREA_FILE_SHOW" => "file",
 		"AREA_FILE_SUFFIX" => "inc",
-		"EDIT_TEMPLATE" => ""
-	)
-);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+		"EDIT_TEMPLATE" => "",
+		"COMPONENT_TEMPLATE" => ".default",
+		"PATH" => "/include/svayaz.php"
+	),
+	false
+);?>
+		</div>
+		</div>
+		</div>
+	
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
